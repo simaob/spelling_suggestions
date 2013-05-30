7c0dc18767f26958d498a238d6f4367918d0f3b4
Simao Belchior de Castro <simaobelchior@gmail.com>
2013-05-30 21:26:48 +0100

    Added benchmarking test

--- spelling_test.rb
+++ spelling_test.rb
@@ -2,6 +2,7 @@
 
 require "minitest"
 require "minitest/autorun"
+require "minitest/benchmark" #if ENV["BENCH"]
 require "./spelling"
 
 class TestSpelling < Minitest::Test
@@ -14,5 +15,16 @@ class TestSpelling < Minitest::Test
     assert_equal 2, @spelling.lcs("abc", "bsdsc")
     assert_equal 0, @spelling.lcs("", "")
   end
-
 end
+
+class BenchSpelling < Minitest::Benchmark
+  def setup
+    @spelling = Spelling.new("SAMPLE_INPUT.txt")
+  end
+  def bench_my_algorithm
+    assert_performance_linear 0.9999 do |n| # n is a range value
+      @spelling.run
+    end
+  end
+end
+
