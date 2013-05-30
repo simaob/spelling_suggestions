20090240a4c0e231fdfe50bd207121828a46c0b9
Duncan Grant <duncan@handyelephant.com>
2013-05-30 21:13:08 +0100

    testing lcs

--- spelling_test.rb
+++ spelling_test.rb
@@ -1,11 +1,18 @@
 #!/usr/bin/ruby
 
+require "minitest"
 require "minitest/autorun"
+require "./spelling"
 
 class TestSpelling < Minitest::Test
 
   def setup
-    Spelling.new("SAMPLE_INPUT.txt")
+    @spelling = Spelling.new("SAMPLE_INPUT.txt")
+  end
+
+  def test_lcs_returns_correct_value
+    assert_equal 2, @spelling.lcs("abc", "bsdsc")
+    assert_equal 0, @spelling.lcs("", "")
   end
 
 end
